d8435f7cb873e29d2141fc4cabe070e61384079a
Shruthi <shruthi.m@iiht.com>
2020-04-22 17:26:43 +0530

    first commit

---
+++ app/server.js
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2012-2019 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+
+/*eslint-env node*/
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(express.static('./'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
